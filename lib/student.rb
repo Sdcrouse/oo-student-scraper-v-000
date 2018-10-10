4b317dd1d2259198257e015fba5d83ea9a53fe7d
Steven Crouse <sdcrouse91@gmail.com>
2018-10-10 03:31:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,6 @@ class Student
 
   def initialize(student_hash)
     self.add_student_attributes(student_hash)
-    #student_hash.each {|key, value| self.send("#{key}=", value)} 
     @@all << self
   end
 
